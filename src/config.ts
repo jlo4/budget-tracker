046855930ea569749dae52e24d25bae8fad4144c
John <john.love4@gmail.com>
2025-01-03 17:07:57 +0200

    chore: add mongodb client and queries (#7)
    * add transaction page, transaction form dialog, transaction type
    
    * add mongodb client and transactions queries

--- src/config.ts
+++ src/config.ts
@@ -10,11 +10,15 @@ enum DeploymentEnvironment {
 interface ApplicationConfig {
     applicationName: string;
     deploymentEnv: DeploymentEnvironment;
+    mongoDbName: string;
+    mongoDbUri: string;
 }
 
 const config: ApplicationConfig = {
     applicationName: "Budget tracker",
     deploymentEnv: (process.env.DEPLOYMENT_ENV || "development") as DeploymentEnvironment,
+    mongoDbName: process.env.MONGODB_DB_NAME || "",
+    mongoDbUri: process.env.MONGODB_URI || "",
 };
 
 export default config;
