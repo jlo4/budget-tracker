6b24d398f7be71c511d4e5f7ec2a8f2cafb0848d
John Love <john.love4@gmail.com>
2025-01-04 14:08:09 +0200

    use getCollection function for mongodb queries

--- src/backend/mongoDb/client.ts
+++ src/backend/mongoDb/client.ts
@@ -13,6 +13,16 @@ const getClient = async (): Promise<MongoClient> => {
     return client;
 };
 
+const getCollection = async (collection: string) => {
+    try {
+        const client = await getClient();
+        return client.db(config.mongoDbName).collection(collection);
+    } catch (error: Error | unknown) {
+        console.error("MongoDB error: ", error);
+    }
+};
+
 export {
-    getClient
+    getClient,
+    getCollection,
 }
