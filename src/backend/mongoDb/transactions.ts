05294b4a382e8d72e6d933486533025d996427e1
John <john.love4@gmail.com>
2025-01-04 14:08:38 +0200

    use getCollection function for mongodb queries (#9)

--- src/backend/mongoDb/transactions.ts
+++ src/backend/mongoDb/transactions.ts
@@ -1,14 +1,11 @@
 "use server";
 
 import { Transaction } from "@/lib/types/Transaction";
-import { getClient } from "./client";
-import config from "@/config";
+import { getCollection } from "./client";
 
 const insertTransactions = async (transactions: Transaction[]) => {
-    try {
-        const client = await getClient();
-        const db = client.db(config.mongoDbName);
-        const collection = db.collection("transactions");
+    try {        
+        const collection = await getCollection("transactions");
         const transactionsWithISODate = transactions.map((transaction) => {
             // TODO: Should check date format
             const [year, month, day] = transaction.date.split("-").map(Number)
@@ -17,7 +14,11 @@ const insertTransactions = async (transactions: Transaction[]) => {
                 date: new Date(year, month - 1, day)
             }
         });
-        const result = await collection.insertMany(transactionsWithISODate);
+        const result = await collection?.insertMany(transactionsWithISODate);
+        if (!result) {
+            console.error("Error inserting transactions", result);
+            return 0;
+        }
         return result.acknowledged ? result.insertedCount : 0;
     } catch (error) {
         console.error("Error inserting transactions", error);
@@ -26,10 +27,8 @@ const insertTransactions = async (transactions: Transaction[]) => {
 
 const fetchTransactions = async () => {
     try {
-        const client = await getClient();
-        const db = client.db(config.mongoDbName);
-        const collection = db.collection("transactions");
-        const result = await collection.find<Transaction>({}, { projection: { _id: 0 }}).sort({ date: -1 }).toArray();
+        const collection = await getCollection("transactions");
+        const result = await collection?.find<Transaction>({}, { projection: { _id: 0 }}).sort({ date: -1 }).toArray();
         return result;
     } catch (error) {
         console.error("Error fetching transactions", error);
